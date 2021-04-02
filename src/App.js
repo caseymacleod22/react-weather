3445553dc5b2a970f6ef709be199fcce4ad24346
Casey MacLeod <casey@Caseys-MacBook-Pro.local>
2021-04-02 16:14:13 -0400

    displays map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
+import { getCurrentLatLng } from './services/geolocation'
+
 import './App.css';
 import Map from './components/Map/Map';
 
 function App () {
+
+  const [appData, setAppData] = useState({
+    lat: null,
+    lng: null,
+  })
+
+  async function getAppData() {
+    // we need broswer location
+    const {lat, lng} = await getCurrentLatLng() // await is like saying wait until I'm done
+      console.log('got app data')
+    // we need weather data
+    // set component state to the received values
+    setAppData({lat, lng})
+  }
+
+  useEffect(() => {
+    getAppData()
+  }, []) // the empty array prevents an infinite loop.
+
   return (
     <div className='App'>
-      <Map />
-      <header className='App-header'>REACT WEATHER</header>
+      <Map lat={appData.lat} lng={appData.lng}/>
+      <header className='App-header'>
+        REACT WEATHER
+      </header>
     </div>
   );
 }
